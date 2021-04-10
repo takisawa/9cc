4d502160ce5cf193d14f39b3a9003b56671416a0
Hiroaki Takisawa <takisawa@hit-start.jp>
2021-04-10 15:07:49 +0900

    main.cを機能毎に分割

---
+++ 9cc.h
@@ -0,0 +1,68 @@
+#include <assert.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//
+// Tokenizer
+//
+
+typedef enum {
+    TK_RESERVED, // 記号
+    TK_NUM,      // 整数トークン
+    TK_EOF,      // 入力の終わりを表すトークン
+} TokenKind;
+
+// Token type
+typedef struct Token Token;
+struct Token {
+    TokenKind kind; // トークンの型
+    Token *next;    // 次の入力トークン
+    int val;        // kindがTK_NUMの場合、その数値
+    char *str;      // トークン文字列
+    int len;        // トークンの長さ
+};
+
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+bool consume(char *op);
+void expect(char *op);
+int expect_number();
+Token *tokenize(char *input);
+extern Token *token;
+
+//
+// Parser
+//
+
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_EQ,  // ==
+  ND_NE,  // !=
+  ND_LT,  // <
+  ND_LE,  // <=
+  ND_NUM, // Integer
+} NodeKind;
+
+// AST node type
+typedef struct Node Node;
+struct Node {
+    NodeKind kind; // Node kind
+    Node *lhs;     // Left-hand side
+    Node *rhs;     // Right-hand side
+    int val;       // Used if kind == ND_NUM
+};
+
+Node *expr();
+
+//
+// codegen.c
+//
+
+void codegen(Node *node);
---
+++ main.c
@@ -0,0 +1,30 @@
+#include "9cc.h"
+
+// 入力プログラム
+char *user_input;
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "引数の個数が正しくありません\n");
+        return 1;
+    }
+
+    // トークナイズしてパースする
+    user_input = argv[1];
+    token = tokenize(argv[1]);
+    Node *node = expr();
+
+    // アセンブリの前半部分を出力
+    printf(".intel_syntax noprefix\n");
+    printf(".global main\n");
+    printf("main:\n");
+
+    // 抽象構文木を下りながらコード生成
+    codegen(node);
+
+    // スタックトップに式全体の値が残っているはずなので
+    // それをRASにロードして関数からの返り値とする
+    printf("  pop rax\n");
+    printf("  ret\n");
+    return 0;
+}
