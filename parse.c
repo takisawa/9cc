a31aa7c2bf0a995c1d122c1de0e40298cd3516fb
Hiroaki Takisawa <takisawa@hit-start.jp>
2021-05-05 14:19:54 +0900

    Support function call with up to 6 arguments

--- parse.c
+++ parse.c
@@ -418,8 +418,31 @@ static Node *unary(Token **rest, Token *tok) {
     return primary(rest, tok);
 }
 
-// primary = "(" expr ")" | ident args? | num
-// args = "(" ")"
+// funcall = ident "(" (assign ("," assign)*))? ")"
+static Node *funcall(Token **rest, Token *tok) {
+    Token *start = tok;
+    tok = tok->next->next;
+
+    Node head = {};
+    Node *cur = &head;
+
+    while (!equal(tok, ")")) {
+        if (cur != &head) {
+            tok = skip(tok, ",");
+        }
+        cur->next = assign(&tok, tok);
+        cur = cur->next;
+    }
+
+    *rest = skip(tok, ")");
+
+    Node *node = new_node(ND_FUNCALL, start);
+    node->funcname = strndup(start->loc, start->len);
+    node->args = head.next;
+    return node;
+}
+
+// primary = "(" expr ")" | ident func-args? | num
 static Node *primary(Token **rest, Token *tok) {
     if (equal(tok, "(")) {
         Node *node = expr(&tok, tok->next);
@@ -430,10 +453,7 @@ static Node *primary(Token **rest, Token *tok) {
     if (tok->kind == TK_IDENT) {
         // Function call
         if (equal(tok->next, "(")) {
-            Node *node = new_node(ND_FUNCALL, tok);
-            node->funcname = strndup(tok->loc, tok->len);
-            *rest = skip(tok->next->next, ")");
-            return node;
+            return funcall(rest, tok);
         }
 
         Obj *var = find_var(tok);
